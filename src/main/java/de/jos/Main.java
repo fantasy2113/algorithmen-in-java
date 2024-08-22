96a43f18c098107b65cd56f6d14fc8f08c21786e
Julian Osmer <julian.osmer@outlook.de>
2024-08-22 15:57:07 +0200

    update

---
+++ src/main/java/de/jos/Main.java
@@ -0,0 +1,7 @@
+package de.jos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/de/jos/algorithmen/Fibonacci.java
@@ -0,0 +1,4 @@
+package de.jos.algorithmen;
+
+public class Fibonacci {
+}
